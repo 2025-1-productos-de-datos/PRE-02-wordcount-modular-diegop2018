16a2124f11fe20dad8a4f957abe55e24ffbf79b8
diegop2018 <36285502+diegop2018@users.noreply.github.com>
2025-05-03 11:00:34 -0500

    Separacion_Funciones_Archivos
    Separamos las funciones

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -9,28 +9,11 @@ from ._internals.Write_count_words import Write_count_words
 
 
 def main():
-    ## mover a la funcion "read_all_lines"
+
     all_lines = read_all_lines()
-
-    ## mover a "preprocess_lines"
     all_lines = preprocess_lines(all_lines)
-
-    ## mover "split_in_words"
     words = split_in_words(all_lines)
-
-    ## mover a "count_words"
     counter = count_words(words)
-
-    # count the frequency of the words in the files in the input directory
-    # counter = {}
-    # for filename in input_directory_files:
-    #  with open("data/input/" + filename) as f:
-    #        for l in f:
-    #   #         for w in l.split():
-    #               w = w.lower().strip(",.!?")
-    #                counter[w] = counter.get(w, 0) + 1
-
-    # escribir el resultado en un archivo tsv
     Write_count_words(counter)
 
 
